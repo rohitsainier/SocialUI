dcea06ee580d63bce988945866b7e389adc5adc5
rohitsainier <rohitsainier@gmail.com>
2021-02-25 12:33:12 +0530

    Initial Commit

---
+++ SocialUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialUI
+//
+//  Created by Rohit Saini on 25/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
